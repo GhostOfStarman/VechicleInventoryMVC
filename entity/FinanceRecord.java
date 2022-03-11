4911850eff2e88d8bf0db7527d83ecd559500170
Andy Szeto <szeto.sabo@gmail.com>
2022-03-10 17:36:50 -0800

    Add files via upload

--- entity/FinanceRecord.java
+++ entity/FinanceRecord.java
@@ -3,10 +3,14 @@ import java.math.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -17,12 +21,12 @@ public class FinanceRecord{
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="FinanceId")
+	@Column(name="financeId")
 	private int financeId;
 	
 	@NotNull(message="cannot be blank")
 	@Min(value=1, message="cannot be blank")
-	@Column(name="CustomerId")
+	@Column(name="customerId")
 	private int customerId;
 	
 	@NotNull(message="cannot be blank")
@@ -32,8 +36,8 @@ public class FinanceRecord{
 	
 	@NotNull(message="cannot be blank")
 	@Size(min=1, max=15, message="must be between 1-15 Characters")
-	@Column(name="VehicleId")
-	private String vehicleId;
+	@Column(name="vin")
+	private String vehicleIdNumber;
 	
 	@NotNull(message="cannot be blank")
 	@Column(name="VehiclePrice")
@@ -44,24 +48,36 @@ public class FinanceRecord{
 	private int termLength;
 	
 	@NotNull(message="cannot be blank")
-	@Column(name="PaymentsMade")
+	@Column(name="paymentsMade")
 	private int installmentsPaid;
 	
 	@NotNull(message="cannot be blank")
-	@Column(name="InterestRate")
-	private double interestRate;
+	@Column(name="apr")
+	private double apr;
 	
 	@NotNull(message="cannot be blank")
-	@Column(name="DownPayment")
+	@Column(name="downPayment")
+	// @Max(value=(long) this.vehiclePrice)
 	private double downPayment;
 	
 	@NotNull(message="cannot be blank")
-	@Column(name="Principal")
-	private double principal;
+	@Column(name="balance")
+	private double balance;
 	
 	@NotNull(message="cannot be blank")
-	@Column(name="TotalOwed")
-	private double totalDebt;
+	@Column(name="monthlyPmt")
+	private double monthlyPaymentAmount;
+	
+	@NotNull(message="cannot be blank")
+	@Column(name="currConditon")
+	private String condition;
+	
+	@Column(name="paidOff")
+	private boolean paidOff;
+	
+//	@OneToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "financeId")
+//	private Vehicle vehicle;
 
 	// these interest rates are ANNUAL, converted to monthly in calcMonthlyPayments()
 	public static final double[] preownedInterestRates = {20.5, 17.75, 11.25, 6.0, 4.5};
@@ -70,50 +86,52 @@ public class FinanceRecord{
 
 	// ----------------------------------------------------------------------------------- >
 	// Constructors
-
-	public FinanceRecord(int financeId){
-		this.financeId = financeId;
-	}
-
-	public FinanceRecord(int financeId, int customerId, String vehicleId){
-		this.financeId = financeId;
-		this.customerId = customerId;
-		this.vehicleId = vehicleId;
-	}
-
-	// constructor without Vehicle object as parameter
-	public FinanceRecord(int financeId, int customerId, int creditScore, String vehicleId, int termLength, double interestRate, double vehicleCost, double downPayment, double principal, double totalDebt, int installmentsPaid){
-		this.financeId = financeId;
-		this.customerId = customerId;
-		this.creditScore = creditScore;
-		this.vehicleId = vehicleId;
-		this.termLength = termLength;
-		this.interestRate = interestRate;
-		this.vehiclePrice = priceFormat(vehicleCost);
-		this.downPayment = priceFormat(downPayment);
-		this.principal = priceFormat(principal);
-		this.totalDebt = priceFormat(vehicleCost - downPayment);
-		this.installmentsPaid = installmentsPaid;
-	}
 	
-	public FinanceRecord(int financeId, int customerId, Vehicle vehicle, int creditScore, int termLength, double interestRate, double downPayment, double principal, double totalDebt, int installmentsPaid){
+	public FinanceRecord() {}
+	
+	public FinanceRecord(int financeId){
+ 		this.financeId = financeId;
+	}
+
+	public FinanceRecord(int financeId, int customerId, String vehicleIdNumber){
+		this.financeId = financeId;
+		this.customerId = customerId;
+		this.vehicleIdNumber = vehicleIdNumber;
+	}
+
+	
+	public FinanceRecord(int financeId, int customerId, int creditScore, String vehicleIdNumber, int termLength, double apr, double vehiclePrice, 
+			String currCondition, double downPayment, int installmentsPaid){
+		
 		this.financeId = financeId;
 		this.customerId = customerId;
 		this.creditScore = creditScore;
-		this.vehicleId = vehicle.getVehicleIdNumber();
+		this.vehicleIdNumber = vehicleIdNumber.toUpperCase();
+		
 		this.termLength = termLength;
-		this.interestRate = interestRate;
+		this.condition = currCondition.toUpperCase();
 		
-		double price = priceFormat(vehicle.getPrice());
+		double annualRate = 0.0;
+		if(currCondition.equals("NEW")) {
+			annualRate = calcPreownedInterestRate(creditScore);
+		}else {
+			annualRate = calcPristineInterestRate(creditScore);
+		}
+		this.apr = annualRate;
 		
-		this.vehiclePrice = price;
-		this.downPayment = priceFormat(downPayment);
-		this.principal = priceFormat(principal);
-		this.totalDebt = priceFormat(price - downPayment);
 		this.installmentsPaid = installmentsPaid;
+		this.vehiclePrice = priceFormat(vehiclePrice);
+		this.downPayment = priceFormat(downPayment);
+		
+		double loanAmount = vehiclePrice - downPayment;
+		double monthlyPayment = calcMonthlyPayments(loanAmount, annualRate, termLength);
+		
+		this.monthlyPaymentAmount = monthlyPayment;
+		this.balance = termLength * monthlyPaymentAmount;
+		this.paidOff = this.installmentsPaid == termLength;
 	}
 
-	//------------------------------------------------- 
+	// ------------------------------------------------- 
 	// calculation methods:
 
 	public double calcPreownedInterestRate(int score){
@@ -145,32 +163,28 @@ public class FinanceRecord{
 	}
 
 	// calculating amortized loan
-	public double calcMonthlyPayments(double principal, double rate, int term){
-		rate /= 12;
-		double monthlyPayments = principal * ((rate * Math.pow((1 + rate), term)/(Math.pow(1 + rate, term) - 1)));
-		return monthlyPayments;
+	public double calcMonthlyPayments(double principal, double apr, int term){
+		apr /= 12;
+		double monthPayments = principal * ((apr * Math.pow((1 + apr), term)/(Math.pow(1 + apr, term) - 1)));
+		return priceFormat(monthPayments);
 	}
 
-	public boolean isFullyPaidOff(){
-		return installmentsPaid == termLength;
-	}
-
-	public boolean makePayment(){
-		totalDebt -= (this.calcMonthlyPayments(this.principal, this.interestRate, this.termLength)); 
-		installmentsPaid -= 1;
-		if(totalDebt > 0){
-			return true;
-		}
-		return false;
-	}
+//	public boolean makePayment(){
+//		balance -= (this.calcMonthlyPayments(this.principal, this.apr, this.termLength)); 
+//		installmentsPaid -= 1;
+//		if(balance > 0){
+//			return true;
+//		}
+//		return false;
+//	}
+	
+	// ----------------------------------------------------------------------------------- >
+	// Getters/Setters:
 	
 	public static double priceFormat(Double price) {
 	    BigDecimal bdPrice = BigDecimal.valueOf(price);
 	    return bdPrice.setScale(2, RoundingMode.HALF_UP).doubleValue();
 	}
-	
-	// ----------------------------------------------------------------------------------- >
-	// Getters/Setters
 
 	public int getFinanceId() {
 		return financeId;
@@ -196,12 +210,12 @@ public class FinanceRecord{
 		this.creditScore = creditScore;
 	}
 
-	public String getVehicleId() {
-		return vehicleId;
+	public String getVehicleIdNumber() {
+		return vehicleIdNumber;
 	}
 
-	public void setVehicleId(String vehicleId) {
-		this.vehicleId = vehicleId;
+	public void setVehicleIdNumber(String vehicleIdNumber) {
+		this.vehicleIdNumber = vehicleIdNumber;
 	}
 
 	public double getVehiclePrice() {
@@ -212,11 +226,11 @@ public class FinanceRecord{
 		this.vehiclePrice = vehiclePrice;
 	}
 
-	public int gettermLength() {
+	public int getTermLength() {
 		return termLength;
 	}
 
-	public void settermLength(int termLength) {
+	public void setTermLength(int termLength) {
 		this.termLength = termLength;
 	}
 
@@ -228,12 +242,12 @@ public class FinanceRecord{
 		this.installmentsPaid = installmentsPaid;
 	}
 
-	public double getInterestRate() {
-		return interestRate;
+	public double getApr() {
+		return apr;
 	}
 
-	public void setInterestRate(double interestRate) {
-		this.interestRate = interestRate;
+	public void setApr(double apr) {
+		this.apr = apr;
 	}
 
 	public double getDownPayment() {
@@ -244,21 +258,46 @@ public class FinanceRecord{
 		this.downPayment = downPayment;
 	}
 
-	public double getPrincipal() {
-		return principal;
+	public double getBalance() {
+		return balance;
 	}
 
-	public void setPrincipal(double principal) {
-		this.principal = principal;
+	public void setBalance(double balance) {
+		this.balance = balance;
 	}
 
-	public double getTotalDebt() {
-		return totalDebt;
+	public double getMonthlyPaymentAmount() {
+		return monthlyPaymentAmount;
 	}
 
-	public void setTotalDebt(double totalDebt) {
-		this.totalDebt = totalDebt;
+	public void setMonthlyPaymentAmount(double monthlyPaymentAmount) {
+		this.monthlyPaymentAmount = monthlyPaymentAmount;
 	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+	public boolean isPaidOff() {
+		return paidOff;
+	}
+
+	public void setPaidOff(boolean paidOff) {
+		this.paidOff = paidOff;
+	}
+	
+//	public Vehicle getVehicle() {
+//		return vehicle;
+//	}
+//
+//	public void setVehicle(Vehicle vehicle) {
+//		this.vehicle = vehicle;
+//	}
+
 
 }
 	
