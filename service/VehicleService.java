07602af374940292c3422ac7bec34b566751cc1a
Andy Szeto <szeto.sabo@gmail.com>
2022-03-16 01:47:01 -0700

    Add files via upload

--- service/VehicleService.java
+++ service/VehicleService.java
@@ -2,6 +2,7 @@ package com.vehicleinventory.service;
 
 import java.util.List;
 
+import com.vehicleinventory.entity.CustomerAccount;
 import com.vehicleinventory.entity.Vehicle;
 
 public interface VehicleService {
@@ -10,8 +11,16 @@ public interface VehicleService {
 	
 	Vehicle getVehicle(String vin);
 
-	void deleteVehicle(String vin);
-
 	void saveVehicle(Vehicle car);
+	
+	void deleteVehicle(String vin);
+	
+	List<CustomerAccount> getCustomerAccounts();
+	
+	CustomerAccount getCustomerAccount(int id);
+
+	void saveCustomerAccount(CustomerAccount account);
+	
+	void deleteCustomerAccount(int id);
 
 }
--- service/VehicleServiceImp.java
+++ service/VehicleServiceImp.java
@@ -6,7 +6,10 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vehicleinventory.dao.CustomerAccountDAO;
+import com.vehicleinventory.dao.FinanceRecordDAO;
 import com.vehicleinventory.dao.VehicleDAO;
+import com.vehicleinventory.entity.CustomerAccount;
 import com.vehicleinventory.entity.Vehicle;
 
 @Service
@@ -14,7 +17,12 @@ public class VehicleServiceImp implements VehicleService{
 	
 	@Autowired
 	VehicleDAO vehicleDAO;
+	@Autowired
+	CustomerAccountDAO customerAccountDAO;
+	@Autowired
+	FinanceRecordDAO financeRecordDAO;
 	
+	// ----------------------------------------------------------------------------------- >
 	@Override
 	@Transactional
 	public List<Vehicle> getVehicles(){
@@ -39,5 +47,34 @@ public class VehicleServiceImp implements VehicleService{
 	public void deleteVehicle(String vin) {
 		vehicleDAO.deleteVehicle(vin);
 	}
+	
+	// ----------------------------------------------------------------------------------- >
+	@Override
+	@Transactional
+	public List<CustomerAccount> getCustomerAccounts() {
+		return customerAccountDAO.getCustomerAccounts();
+	}
+
+	@Override
+	@Transactional
+	public CustomerAccount getCustomerAccount(int id) {
+		return customerAccountDAO.getCustomerAccount(id);
+	}
+
+	@Override
+	@Transactional
+	public void saveCustomerAccount(CustomerAccount account) {
+		customerAccountDAO.saveCustomerAccount(account);
+		
+	}
+
+	@Override
+	@Transactional
+	public void deleteCustomerAccount(int id) {
+		customerAccountDAO.deleteCustomerAccount(id);
+		
+	}
+	
+	// ----------------------------------------------------------------------------------- >
 	
 }
